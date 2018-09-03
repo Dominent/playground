750bc0a595d3d57822e7b81eb5922a5fabe043c2
Dominent <petromilpavlov@gmail.com>
2018-09-03 23:33:30 +0300

    Add Experimental Templates

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const Server = require('./server');
+const Server = require('./Server');
+const Router = require('./Router');
 
-new Server()
-    .registerRoute('/', (req, res) => { })
-    .registerRoute('/templates/{name}', (req, res) => {
+const fs = require('fs');
+const path = require('path');
+
+let router = new Router()
+    .mapRoute('/', (req, res) => { })
+    .mapRoute('/templates/{name}', (req, res, params) => {
+        let location = path.join(__dirname, 'templates', params.path.name);
+        fs.readFile(location, 'utf8', (err, data) => {
+            console.log(data);
+        });
+
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end();
     })
-    .registerRoute('/templates/{name}:name:id', (req, res, params) => {
+    .mapRoute('/templates/{name}:name:id', (req, res, params) => {
         console.log(params);
     })
-    .registerRoute('/templates/{name}/{family}', (req, res, params) => {
+    .mapRoute('/templates/{name}/{family}', (req, res, params) => {
         console.log(params);
     })
+
+new Server(router)
     .start(8000);
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 class Server {
-    constructor() {
-        this.routes = [];
+    constructor(router) {
+        this._router = router;
 
         this.onRequestCallback = this.onRequestCallback.bind(this);
         this.onSuccessCallback = this.onSuccessCallback.bind(this);
@@ -39,7 +39,8 @@ class Server {
             return queryParams;
         };
 
-        for (let item of this.routes) {
+        let routes = this._router.routes;
+        for (let item of routes) {
             let rexExp = new RegExp(item.route, 'gi');
 
             let isMatch = rexExp.test(url);
@@ -53,71 +54,6 @@ class Server {
 
         res.write(404);
         res.end();
-    }
-
-    registerRoute(url, callback) {
-        if (typeof url !== 'string') {
-            throw new Error('URL must be a string!');
-        }
-
-        if (!url.startsWith('/')) {
-            throw new Error('URL must start with "/" symbol!');
-        }
-
-        if (url.length < 0) {
-            throw new Error('URL cannot be empty!');
-        }
-
-        let pathPattern = '[\\w.]+';
-        let buildQueryPattern = (name) => `[?&]${name}=[\\w.]+`
-        let params = {
-            query: {},
-            path: {}
-        }
-
-        const buildPathToken = (param) => `<${param}></${param}>`;
-        const buildQueryToken = (param) => `[${param}][/${param}]`;
-
-        const replacePathToken = /<\w+><\/\w+>/gi;
-        const replaceQueryToken = /\[\w+\]\[\/\w+\]/gi;
-
-        const clearRouteTokens = (route) => route
-            .replace(replacePathToken, (val) => pathPattern)
-            .replace(replaceQueryToken, (val) => buildQueryPattern(/\[(\w+)\]/gi.exec(val)[1]));
-
-        let route = url
-            .replace(/:[\w.]+/gi, (val) => {
-                let param = val.replace(':', '');
-
-                params.query[param] = undefined;
-
-                return buildQueryToken(param);
-            })
-            .replace(/{[\w.]+}/gi, (val) => {
-                let param = val.replace(/{|}/gi, '');
-
-                params.path[param] = undefined;
-
-                return buildPathToken(param);
-            });
-
-        Object.keys(params.path)
-            .forEach(x => params.path[x] = clearRouteTokens(route
-                .replace(buildPathToken(x),
-                    (val) => `(${pathPattern})`)))
-
-        Object.keys(params.query)
-            .forEach(x => params.query[x] = clearRouteTokens(route
-                .replace(buildQueryToken(x),
-                    (val) => `(${buildQueryToken(x)})`)))
-
-        route = clearRouteTokens(route);
-
-        this.routes.push({ route, url, callback, params });
-
-        this.routes = this.routes.sort((x, y) => x.route.length < y.route.length)
-
-        return this;
     }
 
     start(port) {
