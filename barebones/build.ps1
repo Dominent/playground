56c5b3c5c3d324660b35a011bcd4e20b758e4eea
Dominent <petromilpavlov@gmail.com>
2018-09-13 00:12:15 +0300

    Fighting With Commercial Minifiers

--- barebones/build.ps1
+++ barebones/build.ps1
@@ -1,6 +1,8 @@
 $LIBRARY_NAME = 'barebones.js';
 $PATTERN = '\/\* IF DEV \*\/[\S\s]*\/\* END IF \*\/';
 $BUILD_PATH = "./bin";
+# $MINIFIER_LIBRARY_PATH = "./AjaxMin.dll";
+$MINIFIER_LIBRARY_PATH = "./NUglify.dll";
 
 $Files = @(
     (Get-Content -Path './bootstrap.js' | Out-String ),
@@ -11,19 +13,30 @@ $Files = @(
     (Get-Content -Path './client/ModuleBuilder.js' | Out-String),
     (Get-Content -Path './client/Renderer.js' | Out-String)
     (Get-Content -Path './client/Module.js' | Out-String)
+    (Get-Content -Path './client/StateManager.js' | Out-String)
+    (Get-Content -Path './client/StateContainer.js' | Out-String)
 )
 
 if(!(Test-Path $BUILD_PATH)) {
     New-Item -ItemType directory -Path $BUILD_PATH
 }
 
-$Files -replace  $PATTERN, "" -join "`r`n"  | Out-String | Set-Content "$BUILD_PATH\$LIBRARY_NAME"
+# Load AjaxMin.dll
+# Add-Type -Path $MINIFIER_LIBRARY_PATH
+# $Minifier = New-Object -TypeName Microsoft.Ajax.Utilities.Minifier
+
+$content = $Files -replace  $PATTERN, "" -join "`r`n"  | Out-String;
+
+# $Minifier.MinifyJavaScript($content) | Set-Content "$BUILD_PATH\$LIBRARY_NAME"
+# [NUglify.Uglify]::Js( $content) | Set-Content "$BUILD_PATH\$LIBRARY_NAME"
 
 Copy-Item `
     './server/Router.js', `
     './server/Server.js', `
     './server/TemplateEngine.js' `
     -Destination $BUILD_PATH
+
+
 
 
  
