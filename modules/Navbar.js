f6f40ea65fe70bde0175001f22ea85b809a4bf9a
Dominent <petromilpavlov@gmail.com>
2018-09-17 00:26:39 +0300

    Complete Basic Demo

--- modules/Navbar.js
+++ modules/Navbar.js
@@ -1,15 +1,28 @@
 import TNavbar from '/templates/TNavbar.jshtml';
 import Module from '/barebones/Module.js';
+import { dependencyContainer } from '/barebones/DependencyContainer.js';
 
 class Navbar extends Module {
     constructor(props) {
         super(props)
 
         this.title = props.title;
+        this.person = {
+            email: ''
+        };
+
+        dependencyContainer.get('stateManager')
+            .subscribe('person', this.onPersonChange.bind(this))
+    }
+
+    observables() { return ['person'] }
+
+    onPersonChange(ev) {
+        this.person = ev.value;
     }
 
     render() {
-        return TNavbar([], this.title);
+        return TNavbar([], this.title, this.person.email);
     }
 }
 
