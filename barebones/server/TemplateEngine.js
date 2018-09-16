2877262034796765de48a81d7ecee46bda4e3ada
Dominent <petromilpavlov@gmail.com>
2018-09-16 16:49:28 +0300

    Work In Progress

--- barebones/server/TemplateEngine.js
+++ barebones/server/TemplateEngine.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
 
 const templateWrapper = (name, parameters, body) => {
     return `export default function ${name}(${parameters}) {
@@ -19,28 +19,22 @@ class TemplateEngine {
                 }
 
                 let parameters = new Set();
-                let templateBody = data.split('\n').map(x => x.trim()).filter(Boolean)
-                    .map(x => {
-                        let regex = /{{([\w.]+)}}/ig;
+                let lines = data.split('\n').map(x => x.trim()).filter(Boolean);
 
-                        if (regex.test(x)) {
-                            let matches;
+                let output = [];
+                let imports = [];
+                for (let i = 0; i < lines.length;) {
+                    let line = lines[i];
 
-                            regex.lastIndex = 0;
+                    line = this.parseHtmlExpression(line, parameters, output);
+                    i = this.parseCodeExpression(line, i, output);
+                    line = this.parseImportExpression(line, imports);
+                }
 
-                            while (matches = regex.exec(x)) {
-                                parameters.add(matches[1]);
-
-                                x = x.replace(matches[0], '${' + matches[1] + '}')
-                            }
-
-                            return '__html.push(`' + x + '`);';
-                        }
-
-                        return '__html.push(`' + x + '`);';
-                    }).join('\n');
+                let templateBody = output.join('\n');
 
                 return resolve(
+                    imports.join('\n') +
                     templateWrapper(
                         path.basename(filename, '.jshtml'),
                         ['__html', ...parameters].join(', '),
@@ -48,6 +42,51 @@ class TemplateEngine {
                 )
             })
         })
+    }
+
+    parseImportExpression(line, imports) {
+        let importExpressionRegEx = /{\(([\w\s.\/;']+)\)}/ig;
+        if (importExpressionRegEx.test(line)) {
+            let matches;
+            importExpressionRegEx.lastIndex = 0;
+            while (matches = importExpressionRegEx.exec(line)) {
+                line = line.replace(matches[0], matches[1]);
+            }
+            imports.push(line);
+        }
+        return line;
+    }
+
+    parseCodeExpression(line, i, output) {
+        let startRegEx = /{\[/;
+        let endRegEx = /\]}/;
+        if (startRegEx.test(line)) {
+            let code = line.replace(startRegEx, '');
+            do {
+                ++i;
+            } while (!endRegEx.test(code));
+            code = code.replace(endRegEx, '');
+            output.push(code);
+        }
+        else {
+            ++i;
+            output.push('__html.push(`' + line + '`);');
+        }
+        return i;
+    }
+
+    parseHtmlExpression(line, parameters, output) {
+        let htmlExpressionRegEx = /{{([\w.]+)}}/ig;
+        if (htmlExpressionRegEx.test(line)) {
+            let matches;
+            htmlExpressionRegEx.lastIndex = 0;
+            while (matches = htmlExpressionRegEx.exec(line)) {
+                parameters.add(matches[1]);
+                line = line.replace(matches[0], '${' + matches[1] + '}');
+            }
+            output.push('__html.push(`' + line + '`);');
+        }
+        return line;
     }
 }
 
